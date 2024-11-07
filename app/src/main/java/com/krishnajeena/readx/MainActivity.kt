8190f44900f14430f5d6dd2cc0284d15d92796dd
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2024-11-07 22:41:50 +0530

    Initial commit

---
+++ app/src/main/java/com/krishnajeena/readx/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.krishnajeena.readx
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.krishnajeena.readx.ui.theme.ReadXTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ReadXTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                  
+                }
+            }
+        }
+    }
+}
